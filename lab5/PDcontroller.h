4b4c9860920adb17003a1a06e93be90932cb5545
Joshua Van Doren <j4vdoren@gmail.com>
2025-02-26 19:48:39 -0800

    lab5 draftcode

---
+++ lab5/PDcontroller.h
@@ -0,0 +1,22 @@
+#ifndef PDcontroller_h
+#define PDcontroller_h
+#include <Pololu3piPlus32U4.h>
+using namespace Pololu3piPlus32U4;
+
+class PDcontroller {
+  public:
+    PDcontroller(float kp, float kd, double minOutput, double maxOutput);
+    double update(double value, double target_value);
+
+  private:
+    float _kp;               // Proportional gain
+    float _kd;               // Derivative gain
+    double _minOutput;       // Minimum output limit
+    double _maxOutput;       // Maximum output limit
+    double _error;           // Difference between target and current value
+    double _prevError;       // Previous error for derivative calculation
+    double _output;          // Raw controller output
+    double _clampOut;        // Clamped controller output
+};
+
+#endif
